fed77ef2c7b211fe56acc3f64b075189e7a4c8ae
Pablo Martin <elektrochose@gmail.com>
2023-02-27 20:44:56 -0600

    reverting

--- download_models.sh
+++ download_models.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 
-wget https://drive.google.com/uc?id=1-LKJTZakqd8ntKzqVNtQZUgdZnFoYtpK
+gdown https://drive.google.com/uc?id=1-LKJTZakqd8ntKzqVNtQZUgdZnFoYtpK
 unzip Contract_Embedding.zip
 cp -r Embedding/ contract_level/
 rm -rf Embedding/
 
-wget https://drive.google.com/uc?id=1lbaQVtZbNuEEjHIWVnwLqGvILxNWwtZW  
+gdown https://drive.google.com/uc?id=1lbaQVtZbNuEEjHIWVnwLqGvILxNWwtZW  
 unzip Contract_Model.zip  
 mv Model contract_level/
 rm -rf Model
 
-wget https://drive.google.com/uc?id=18GiDgSwoRjPC25d2Vp15oi_xH2NivyXH  
+gdown https://drive.google.com/uc?id=18GiDgSwoRjPC25d2Vp15oi_xH2NivyXH  
 unzip Statement_Model.zip 
 mv Model statement_level/  
 rm -rf Model
