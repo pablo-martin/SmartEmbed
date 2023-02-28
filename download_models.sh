09dfbd4fcb175d881cb9310ca8ca035653e38852
Pablo Martin <elektrochose@gmail.com>
2023-02-27 20:39:50 -0600

    switched from gdown to wget

--- download_models.sh
+++ download_models.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 
-gdown https://drive.google.com/uc?id=1-LKJTZakqd8ntKzqVNtQZUgdZnFoYtpK
+wget https://drive.google.com/uc?id=1-LKJTZakqd8ntKzqVNtQZUgdZnFoYtpK
 unzip Contract_Embedding.zip
 cp -r Embedding/ contract_level/
 rm -rf Embedding/
 
-gdown https://drive.google.com/uc?id=1lbaQVtZbNuEEjHIWVnwLqGvILxNWwtZW  
+wget https://drive.google.com/uc?id=1lbaQVtZbNuEEjHIWVnwLqGvILxNWwtZW  
 unzip Contract_Model.zip  
 mv Model contract_level/
 rm -rf Model
 
-gdown https://drive.google.com/uc?id=18GiDgSwoRjPC25d2Vp15oi_xH2NivyXH  
+wget https://drive.google.com/uc?id=18GiDgSwoRjPC25d2Vp15oi_xH2NivyXH  
 unzip Statement_Model.zip 
 mv Model statement_level/  
 rm -rf Model
